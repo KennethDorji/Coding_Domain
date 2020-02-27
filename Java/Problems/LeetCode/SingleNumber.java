ce3f2ac3a9ca63e36c8628444d5f58bdc85c3744
Kenneth Dorji <kennethdorji@gmail.com>
2020-02-26 22:37:32 -0500

    Create SingleNumber.java

---
+++ Java/Problems/LeetCode/SingleNumber.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class SingleNumber {
+
+    public static int singleNumber(int[] nums) {
+
+        HashMap<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (hm.containsKey(nums[i]))
+                hm.put(nums[i], hm.get(nums[i]) + 1);
+            else
+                hm.put(nums[i], 1);
+        }
+
+        int singleNum = 0;
+        for (Entry<Integer, Integer> key : hm.entrySet()) {
+            if (key.getValue() % 2 == 1) {
+                singleNum = key.getKey();
+                break;
+            }
+        }
+        return singleNum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[] { 4, 1, 2, 1, 4, 3, 2 };
+
+        System.out.println(singleNumber(nums));
+
+        System.exit(0);
+    }
+}
